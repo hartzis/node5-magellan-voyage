15ba513e6365816d831cffc5ee8115a2cd5b8fc2
Brian Emil Hartz <brianhartz@gmail.com>
2014-05-07 15:48:44 -0600

    all magellan sites working

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('index');
+    res.render('seville');
+});
+app.get('/canary', function(req, res) {
+    res.render('canary');
+});
+app.get('/verde', function(req, res) {
+    res.render('verde');
+});
+app.get('/som', function(req, res) {
+    res.render('som');
+});
+app.get('/guam', function(req, res) {
+    res.render('guam');
+});
+app.get('/death', function(req, res) {
+    res.render('death');
 });
 
 var server = app.listen(7482, function() {
-	console.log('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
 });
